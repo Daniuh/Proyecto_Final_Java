2b319df2995acb9020ff0db6df0b565c941ba04a
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-02-12 11:54:01 -0500

    Se agrego el punto 3

---
+++ src/punto3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class punto3 {
+    public static void main(String[] args) {
+        Scanner dato1 = new Scanner(System.in);
+        System.out.println("Ingresa el raidio");
+        String radioString = dato1.nextLine();
+        double radio = Double.parseDouble(radioString);
+        System.out.println("El área del circulo es: "+Math.PI * Math.pow (radio, 2));
+    }
+}
