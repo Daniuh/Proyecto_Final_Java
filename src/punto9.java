a9617c64fb36dd6e37233379b128b7b5ac9e998e
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-02-12 17:42:43 -0500

    En este commit se agrego el punto 6

---
+++ src/punto9.java
@@ -0,0 +1,8 @@
+public class punto9 {
+    public static void main(String[] args) {
+        String cadena="La sonrisa sera la mejor arma contra la tristeza";
+        System.out.print(cadena.replace('a', 'e')+" y la sabiduria la mejor arma contra la estupidez");
+
+
+    }
+}
