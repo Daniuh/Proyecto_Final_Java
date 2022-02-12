da5003569f45d343a1bcce04f6d0fe6e6e073545
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-02-12 12:46:53 -0500

    Se agrego el punto 4

---
+++ src/punto4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class punto4 {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+        System.out.println("Ingrese el precio");
+        double precio = numero.nextDouble();
+        final double iva = (21 * precio) / 100;
+        System.out.println("El iva del precio: "+precio+" es: "+iva);
+    }
+}
